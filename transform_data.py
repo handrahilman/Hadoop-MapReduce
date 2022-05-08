5dd27d2eb6fe687771ef05deea88dab107f9d188
Handra Hilman Yunus <handrahilman@gmail.com>
2022-05-08 15:45:37 +0700

    add transform method

---
+++ transform_data.py
@@ -0,0 +1,31 @@
+#!python3
+
+import pandas as pd
+import model
+import connection
+
+from sqlalchemy import create_engine
+from hdfs import InsecureClient
+from datetime import datetime
+
+if __name__ == "__main__":
+    time = datetime.now().strftime("%Y%m%d")
+
+    engine = create_engine('postgresql://postgres:542104372qwertY!@localhost:5432/dwh_digitalskola')
+
+    conf_hadoop = connection.param_config("hadoop")["ip"]
+    client = InsecureClient(conf_hadoop)
+
+    with client.read(f'/DigitalSkola/{time}/users_{time}.csv', encoding='utf-8') as writer:
+        df = pd.read_csv(writer)
+    df.to_sql("dwh_dim_users", engine, if_exists='replace', index=False)
+
+    conf_postgresql = connection.param_config("postgresql")
+    conn = connection.postgres_conn(conf_postgresql)
+    cur = conn.cursor()
+
+    sql = model.dwh_fact_orders()
+    cur.execute(sql)
+    data = cur.fetchall()
+    df = pd.DataFrame(data, columns=[col[0] for col in cur.description])
+    df.to_sql("dwh_fact_orders", engine, if_exists='replace', index=False)
